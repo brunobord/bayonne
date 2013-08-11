58073cb92a7bfda2c7a89f77af85b939067a92e5
Bruno Bord <bruno@jehaisleprintemps.net>
2013-08-11 23:30:52 +0200

    better template

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 import codecs
+import datetime
 
 import markdown
 from jinja2 import Template
+
+__version__ = '0.1'
 
 text = codecs.open("index.md", mode="r", encoding="utf-8").read()
 html = markdown.markdown(text, extensions=['headerid'])
@@ -11,4 +14,4 @@ html = markdown.markdown(text, extensions=['headerid'])
 template = Template(codecs.open('template.html', mode='r', encoding="utf-8").read())
 
 with codecs.open('www/index.html', mode="w", encoding="utf-8") as fd:
-    fd.write(template.render(content=html))
+    fd.write(template.render(content=html, date=datetime.date.today(), version=__version__))
