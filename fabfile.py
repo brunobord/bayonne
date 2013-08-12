2e7b91bb06f37bd971dc6f5e8a18397f1ccc4f69
Bruno Bord <bruno@jehaisleprintemps.net>
2013-08-12 22:25:30 +0200

    ok, quelques scripts

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import codecs
+import json
+
+import requests
+from fabric.api import local, task
+
+
+@task
+def build():
+    "Build the website"
+    local('python build.py')
+
+
+@task
+def download():
+    "Download the GeoJSON map"
+    req = requests.get('https://gist.github.com/brunobord/6206708/raw/map.geojson')
+    data = json.loads(req.text)
+    with codecs.open('map.geojson', mode="w", encoding="utf-8") as geojson:
+        geojson.write(json.dumps(data, indent=3))
